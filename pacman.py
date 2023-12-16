47f7393fbff4b207737b0a064a6bdb72a0fc5475
Connor Thomas <78455694+ConnorNoelThomas@users.noreply.github.com>
2023-12-15 21:06:04 -0500

    Add draw board logic.

--- pacman.py
+++ pacman.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 from board import boards
 
@@ -5,11 +6,14 @@ pygame.init()
 
 WIDTH = 900
 HEIGHT = 950
+PI = math.pi
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesansbold.ttf', 20)
 level = boards[0]
+background_color = 'black'
+ghost_door_color = 'white'
 primary_color = 'white'
 secondary_color = 'blue'
 
@@ -30,32 +34,33 @@ def draw_board(level):
                     screen, secondary_color,
                     (j * tile_width + (0.5 * tile_width), i * tile_height),
                     (j * tile_width + (0.5 * tile_width), i * tile_height + tile_height), 3)
-            if level[i][j] == 4:
+            if (level[i][j] == 4) | (level[i][j] == 9):
                 pygame.draw.line(
-                    screen, secondary_color,
+                    screen, secondary_color if level[i][j] == 4 else ghost_door_color,
                     (j * tile_width, i * tile_height + (0.5 * tile_height)),
                     (j * tile_width + tile_width, i * tile_height + (0.5 * tile_height)), 3)
-            # if level[i][j] == 5:
-            #     pygame.draw.circle(
-            #         screen, 'white', (j * tile_width + (0.5 * tile_width), i * tile_height + (0.5 * tile_height)), 4)
-            # if level[i][j] == 6:
-            #     pygame.draw.circle(
-            #         screen, 'white', (j * tile_width + (0.5 * tile_width), i * tile_height + (0.5 * tile_height)), 10)
-            # if level[i][j] == 7:
-            #     pygame.draw.circle(
-            #         screen, 'white', (j * tile_width + (0.5 * tile_width), i * tile_height + (0.5 * tile_height)), 4)
-            # if level[i][j] == 8:
-            #     pygame.draw.circle(
-            #         screen, 'white', (j * tile_width + (0.5 * tile_width), i * tile_height + (0.5 * tile_height)), 4)
-            # if level[i][j] == 9:
-            #     pygame.draw.circle(
-            #         screen, 'white', (j * tile_width + (0.5 * tile_width), i * tile_height + (0.5 * tile_height)), 4)
+            if level[i][j] == 5:
+                pygame.draw.arc(
+                    screen, secondary_color,
+                    [(j * tile_width - (tile_width * 0.4)) - 2, (i * tile_height + (0.5 * tile_height)), tile_width, tile_height], 0, PI/2, 3)
+            if level[i][j] == 6:
+                pygame.draw.arc(
+                    screen, secondary_color,
+                    [(j * tile_width + (tile_width * 0.5)), (i * tile_height + (0.5 * tile_height)), tile_width, tile_height], PI/2, PI, 3)
+            if level[i][j] == 7:
+                pygame.draw.arc(
+                    screen, secondary_color,
+                    [(j * tile_width + (tile_width * 0.5)), (i * tile_height - (0.4 * tile_height)), tile_width, tile_height], PI, 3*PI/2, 3)
+            if level[i][j] == 8:
+                pygame.draw.arc(
+                    screen, secondary_color,
+                    [(j * tile_width - (tile_width * 0.4)) - 2, (i * tile_height - (0.4 * tile_height)), tile_width, tile_height], 3*PI/2, 2*PI, 3)
 
 
 run = True
 while run:
     timer.tick(fps)
-    screen.fill('black')
+    screen.fill(background_color)
     draw_board(level)
 
     for event in pygame.event.get():
